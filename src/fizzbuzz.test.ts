800c9483b15e3f0324239f4ef9cdf39cc00630ab
Lita Garcia <manuelagarciayebra@gmail.com>
2024-01-27 20:16:28 +0100

    REFACTOR: Add types to test

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -4,47 +4,50 @@ import { fizzbuzz } from "./fizzbuzz.ts";
 describe("fizzbuzz", () => {
   it("should return a 0 inside a string list when input is 0", () => {
     //given
-    const inputNumber = 0;
+    const inputNumber: number = 0;
 
     //when
-    const result = fizzbuzz(inputNumber);
+    const result: string[] = fizzbuzz(inputNumber);
 
     //then
     expect(result).toEqual(["0"]);
   });
   it("should return a 1 inside a string list when input is 1", () => {
     //given
-    const inputNumber = 1;
+    const inputNumber: number = 1;
 
     //when
-    const result = fizzbuzz(inputNumber);
+    const result: string[] = fizzbuzz(inputNumber);
 
     //then
     expect(result).toEqual(["0", "1"]);
   });
   it("should return fizz when is multiple of 3", () => {
-    const inputNumber = 3;
+    //given
+    const inputNumber: number = 3;
 
     //when
-    const result = fizzbuzz(inputNumber);
+    const result: string[] = fizzbuzz(inputNumber);
 
     //then
     expect(result).toEqual(["0", "1", "2", "fizz"]);
   });
   it("should return buzz when is multiple of 5", () => {
-    const inputNumber = 5;
+    //given
+    const inputNumber: number = 5;
 
     //when
-    const result = fizzbuzz(inputNumber);
+    const result: string[] = fizzbuzz(inputNumber);
 
     //then
     expect(result).toEqual(["0", "1", "2", "fizz", "4", "buzz"]);
   });
   it("should return fizzbuzz when is multiple of 5 and 3", () => {
-    const inputNumber = 15;
+    //given
+    const inputNumber: number = 15;
 
     //when
-    const result = fizzbuzz(inputNumber);
+    const result: string[] = fizzbuzz(inputNumber);
 
     //then
     expect(result).toEqual([
