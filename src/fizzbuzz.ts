2bb172abebf2e05cdb656f1399a22f02f2c8681b
Lita Garcia <manuelagarciayebra@gmail.com>
2024-01-27 19:42:38 +0100

    FEAT: Modify method to add buzz condition

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -3,6 +3,8 @@ export function fizzbuzz(n: number): string[] {
   for (let i = 0; i <= n; i++) {
     if (i % 3 === 0 && i !== 0) {
       fizzbuzzList.push("fizz");
+    } else if (i % 5 === 0 && i !== 0) {
+      fizzbuzzList.push("buzz");
     } else {
       fizzbuzzList.push(i.toString());
     }
